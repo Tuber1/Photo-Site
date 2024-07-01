f864ef029091648dacd361e309f76461522652e1
Tuber1 <37583758+Tuber1@users.noreply.github.com>
2024-06-30 21:23:40 -0500

    update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const imageGrid = document.getElementById('imageGrid');
+    const baseURL = 'https://github.com/Tuber1/Photo-Site/tree/main/public/Images';
 
     fetch('/api/images')
         .then(response => response.json())
         .then(images => {
             images.forEach(filename => {
                 const img = document.createElement('img');
-                img.src = `images/${filename}`;
+                img.src = `${baseURL}${filename}`;
                 imageGrid.appendChild(img);
             });
         })
