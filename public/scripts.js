e99229e3d6d258098a702502ad5d86fb38370d15
Tuber1 <37583758+Tuber1@users.noreply.github.com>
2024-06-30 22:26:15 -0500

    adjust fetch

--- public/scripts.js
+++ public/scripts.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const imageGrid = document.getElementById('imageGrid');
     const baseURL = 'https://raw.githubusercontent.com/Tuber1/Photo-Site/main/public/images/';
 
-    fetch('/api/images')
+    fetch('/public/images')
         .then(response => response.json())
         .then(images => {
             images.forEach(filename => {
